e389b7df5f1fd7446644e8e32658d36f7e301bb1
MuhammadAffan623 <affanmuhammad623@gmail.com>
2025-01-08 03:23:21 +0500

    updated reward schema

--- src/models/rewardPrice.ts
+++ src/models/rewardPrice.ts
@@ -1,23 +1,26 @@
 import { Schema, model, Document } from "mongoose";
 
 export interface IRewardPrice extends Document {
-  impressions: number;
-  posts: number;
-  comments: number;
-  retweets: number;
-  spacesAttended: number;
-  telegramMessages: number;
+  // impressions: number;
+  // posts: number;
+  // comments: number;
+  // retweets: number;
+  // spacesAttended: number;
+  // telegramMessages: number;
+  price: number;
+  createdAt?: Date;
   updatedAt?: Date;
 }
 
 const rewardPriceSchema = new Schema<IRewardPrice>(
   {
-    impressions: { type: Number, default: 0 },
-    posts: { type: Number, default: 0 },
-    comments: { type: Number, default: 0 },
-    retweets: { type: Number, default: 0 },
-    spacesAttended: { type: Number, default: 0 },
-    telegramMessages: { type: Number, default: 0 },
+    // impressions: { type: Number, default: 0 },
+    // posts: { type: Number, default: 0 },
+    // comments: { type: Number, default: 0 },
+    // retweets: { type: Number, default: 0 },
+    // spacesAttended: { type: Number, default: 0 },
+    // telegramMessages: { type: Number, default: 0 },
+    price: { type: Number, default: 0 },
   },
   { timestamps: true }
 );

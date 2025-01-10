a072393c911b9d950e7a7b7aa2a89405fb46725e
MuhammadAffan623 <affanmuhammad623@gmail.com>
2025-01-10 22:37:49 +0500

    updated reward pricing as every activity have diff reward

--- src/models/rewardPrice.ts
+++ src/models/rewardPrice.ts
@@ -1,26 +1,24 @@
 import { Schema, model, Document } from "mongoose";
 
 export interface IRewardPrice extends Document {
-  // impressions: number;
-  // posts: number;
-  // comments: number;
-  // retweets: number;
-  // spacesAttended: number;
-  // telegramMessages: number;
-  price: number;
+  impressionReward: number;
+  tweetsReward: number;
+  retweetsReward: number;
+  spacesAttendedReward: number;
+  telegramReward: number;
+  // price: number;
   createdAt?: Date;
   updatedAt?: Date;
 }
 
 const rewardPriceSchema = new Schema<IRewardPrice>(
   {
-    // impressions: { type: Number, default: 0 },
-    // posts: { type: Number, default: 0 },
-    // comments: { type: Number, default: 0 },
-    // retweets: { type: Number, default: 0 },
-    // spacesAttended: { type: Number, default: 0 },
-    // telegramMessages: { type: Number, default: 0 },
-    price: { type: Number, default: 0 },
+    impressionReward: { type: Number, default: 0 },
+    tweetsReward: { type: Number, default: 0 },
+    retweetsReward: { type: Number, default: 0 },
+    spacesAttendedReward: { type: Number, default: 0 },
+    telegramReward: { type: Number, default: 0 },
+    // price: { type: Number, default: 0 },
   },
   { timestamps: true }
 );

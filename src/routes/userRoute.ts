034ebcadfcd26cf0deb9ca22e2e56988bb1e4bfb
MuhammadAffan623 <affanmuhammad623@gmail.com>
2025-01-16 20:57:30 +0500

    potexted user whotelist api

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import userController from "../controller/userController";
-import { decodeTokenFromRequest } from "../utils";
+import { authenticateAdmin, decodeTokenFromRequest } from "../utils";
 import { upload } from "../utils/multer";
 const user = userController();
 const userRouter = Router();
 
 userRouter.post("/", user.getOrCreateUser);
 userRouter.get("/", decodeTokenFromRequest, user.getUserbyToken);
-// userRouter.post("/admin", user.createAdmin);
-userRouter.post("/whiteList",upload.single("file"), user.whiteListUser)
+userRouter.post(
+  "/whiteList",
+  authenticateAdmin,
+  upload.single("file"),
+  user.whiteListUser
+);
 
 export default userRouter;
